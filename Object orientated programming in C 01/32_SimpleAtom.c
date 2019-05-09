fbfe10edb5ac6979d8b056e7e8ea534054f2fa3c
mrmarkuese <markus.haedrich@mail.de>
2019-05-09 17:46:02 +0200

    clean up

---
+++ Object orientated programming in C 01/32_SimpleAtom.c
@@ -0,0 +1,184 @@
+
+/*************************************************************/
+/**
+ *  @file SimpleAtom.c
+ *  @brief Example 1/2 how to implement overloading in C
+ *  @author Thomas Resch, thomas.resch@campus.tu-berlin.de\n
+ *          Markus Hädrich, markus.haedrich@tu-berlin.de
+ *
+ *  @discussion Course: Real-Time Audio Programing in C\n
+ *  Supervisors: Thomas Resch, Markus Hädrich, (TU-Berlin)\n
+ *
+ *  However, overloading for your own functions is not supported in C.
+ *  There are different approaches for achieving this kind of behavior,
+ *  all have advantages and disadvantages, depending on what you are trying
+ *  to do. This is one possible example.
+ *
+ *  @date 09.05.19. – first implementation
+ *
+ *  @bug if you found one - report it!
+ *
+ *  @version 0.42
+ **************************************************************/
+
+
+#include <stdio.h>
+
+/*
+ 
+ Pure Data uses a different approach to achieve similar behaviour.
+ It defines a new data type:
+ 
+ t_atom
+ 
+ The t_atom struct contains a member for a float, for a symbol and for
+ several other things. It also contains a member for its type and
+ corresponding setter functions (implemented as macros), for example
+ 
+ SETFLOAT(atom, f)
+ 
+ and getter functions
+ 
+ t_float atom_getfloat(t_atom *a)
+ 
+ When SETFLOAT is called, the atom’s type is set to float and the value f
+ stored in the float member of the atom struct. When atom_getfloat() is called,
+ the atom’s type is checked and if it is indeed a float it returns the float
+ member of the atom. Among other things this technique allows for working with
+ lists of mixed data (usually strings and floats).
+ 
+ 
+ 
+ 
+ Task: Pure Data works internally with floats only, but let’s implement our
+ own simple atom type which contains three members: the type (int or float as
+ an enum value), and a member for a float and a member for an integer. Write
+ the corresponding getter and setter functions and a function, that adds all
+ elements of an atom array together.
+ 
+ */
+
+
+
+
+/**
+ * @enum Enum that holds the defined data types.
+ */
+enum simpleAtomTypes {UNDEFINED, INTEGER, FLOAT};
+
+/**
+ * @brief A simple atom example.
+ * @typedef A simple atom object type.
+ */
+typedef struct simpleAtom
+{
+    int type;           /**< @c int variable, that defines the atom type. */
+    int intVal;         /**< @c int variable. */
+    float floatVal;     /**< @c float variable. */
+} simpleAtom;
+
+
+/**
+ * @brief Function that sets the @c float var of a atom type.
+ * @param a Pointer to an atom type object.
+ * @param fVal @c float var
+ */
+void simpleAtom_setFloat(simpleAtom *a, float fVal)
+{
+    a->type = FLOAT;
+    a->floatVal = fVal;
+}
+
+/**
+ * @brief Function that sets the @c int var of a atom type.
+ * @param a Pointer to an atom type object.
+ * @param iVal @c int var
+ */
+void simpleAtom_setInt(simpleAtom *a, int iVal)
+{
+    a->type = INTEGER;
+    a->intVal = iVal;
+}
+
+
+/**
+ * @brief Function that gets the @c float var of a atom type.
+ * @param a Pointer to an atom type object.
+ * @return @c float var of the atom type.
+ */
+float simpleAtom_getFloat(simpleAtom *a)
+{
+    if(a->type == FLOAT)
+        return a->floatVal;
+    else
+    {
+        printf("Atom does not contain a float value!\n");
+        return 0;
+    }
+}
+
+/**
+ * @brief Function that gets the @c int var of a atom type.
+ * @param a Pointer to an atom type object.
+ * @return @c int var of the atom type.
+ */
+int simpleAtom_getInt(simpleAtom *a)
+{
+    if(a->type == INTEGER)
+        return a->intVal;
+    else
+    {
+        printf("Atom does not contain a int value!\n");
+        return 0;
+    }
+}
+
+
+/**
+ * @brief Function that adds the elements of a atom type.
+ * @param a Pointer to an atom type object.
+ * @param size Length of the atom type.
+ * @return @c int var of the atom type.
+ */
+float simpleAtom_addAll(simpleAtom *a, int size)
+{
+    float sum = 0;
+    for(int i=0; i<size;i++)
+    {
+        if(a[i].type == FLOAT)
+            sum+=a[i].floatVal;
+        if(a[i].type == INTEGER)
+            sum+=a[i].intVal;
+    }
+    
+    return sum;
+}
+
+int main() {
+    // Length of the atom type
+    const int size = 2;
+    
+    // Var to hold the result
+    float result = 0;
+    
+    // Declare a atom type
+    simpleAtom myAtom[size];
+    
+    // Initialize the float var
+    simpleAtom_setFloat(&myAtom[0], 5.5);
+    // Initialize the int var
+    simpleAtom_setInt(&myAtom[1], 67);
+    
+    // Call the function that adds all elements of the atom type
+    result = simpleAtom_addAll(myAtom, size);
+    
+    // Print the result
+    printf("%f\n", result);
+    
+    return 0;
+}
+
+/*
+ Output:
+ 72.500000
+ */
